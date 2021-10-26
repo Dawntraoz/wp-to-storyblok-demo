4aa0d1564d6eb3c450f0a245f4c5eabc4979e21b
Dawntraoz <albasilventefu@gmail.com>
2021-10-26 15:48:23 +0200

    feat: run npx tailwindcss init to get tailwind.config.js and some default heading styles.

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,4 +27,32 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
+
+/* Global styles */
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply font-bold;
+}
+
+h1 {
+  @apply text-4xl;
+}
+
+h2 {
+  @apply text-3xl;
+}
+
+h3 {
+  @apply text-2xl;
+}
+
+h4,
+h5,
+h6 {
+  @apply text-xl;
+}
 </style>
