81424754815dcd96f24d1d7dce3e7694c034358f
Dawntraoz <albasilventefu@gmail.com>
2021-10-27 18:15:33 +0200

    fix: add slash to the dynamic links

--- components/MoleculeArticle.vue
+++ components/MoleculeArticle.vue
@@ -7,7 +7,7 @@
         <span class="text-blue-500">{{ new Intl.DateTimeFormat('en-EN', { day: 'numeric', month: 'long', year: 'numeric' }).format(new Date(article.first_published_at)) }}</span>
       </p>
     </header>
-    <nuxt-link :to="article.full_slug">
+    <nuxt-link :to="`/${article.full_slug}`">
       <atom-image
         v-if="article.content.featured_image"
         :image="article.content.featured_image"
@@ -17,7 +17,7 @@
     </nuxt-link>
     <div class="max-w-prose mx-auto">
       <atom-rich-text :rich-text="article.content.excerpt"></atom-rich-text>
-      <nuxt-link :to="article.full_slug" class="block py-4">Read more >></nuxt-link>
+      <nuxt-link :to="`/${article.full_slug}`" class="block py-4">Read more >></nuxt-link>
       <p>
         Posted in
         <nuxt-link v-for="category in article.content.categories" :key="`category-${category.id}`" :to="`/${category.full_slug}`">
